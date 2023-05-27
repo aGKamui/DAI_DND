1a43aa5bde7f4708ce4163c22991b882097071af
RuiRabaca3391 <50949693+RuiRabaca3391@users.noreply.github.com>
2023-05-27 23:58:10 +0000

    Para não me arrepender

--- frontend/src/layout/Sidebar/index.js
+++ frontend/src/layout/Sidebar/index.js
@@ -1,16 +1,3 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
 import PropTypes from 'prop-types';
 
 // material-ui
@@ -22,32 +9,16 @@ import PerfectScrollbar from 'react-perfect-scrollbar';
 import { BrowserView, MobileView } from 'react-device-detect';
 
 // project imports
-import MenuList from './layout/Sidebar/MenuList';
-import LogoSection from './LogoSection';
+import MenuList from './MenuList';
+import LogoSection from '../LogoSection';
 import { drawerWidth } from './store/constant';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="">
-        Ducks & Dragons 
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+// ==============================|| SIDEBAR DRAWER ||============================== //
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
-export default function Sidebars() {
-  const Sidebar = ({ drawerOpen, drawerToggle, window }) => {
+const Sidebar = ({ drawerOpen, drawerToggle, window }) => {
     const theme = useTheme();
     const matchUpMd = useMediaQuery(theme.breakpoints.up('md'));
-  
+
     const drawer = (
         <>
             <Box sx={{ display: { xs: 'block', md: 'none' } }}>
@@ -74,9 +45,9 @@ export default function Sidebars() {
             </MobileView>
         </>
     );
-  
+
     const container = window !== undefined ? () => window.document.body : undefined;
-  
+
     return (
         <Box component="nav" sx={{ flexShrink: { md: 0 }, width: matchUpMd ? drawerWidth : 'auto' }} aria-label="mailbox folders">
             <Drawer
@@ -103,20 +74,12 @@ export default function Sidebars() {
             </Drawer>
         </Box>
     );
-  };
-}
+};
 
+Sidebar.propTypes = {
+    drawerOpen: PropTypes.bool,
+    drawerToggle: PropTypes.func,
+    window: PropTypes.object
+};
 
-//function App() {
-//  return (
-//    <BrowserRouter>
-//      <div>
-//        <Routes>
-//          <Route path="/" element={<Home />} />
-//        </Routes>
-//      </div>
-//    </BrowserRouter>
-//  );
-//}
-
-// export default App;
+export default Sidebar;
