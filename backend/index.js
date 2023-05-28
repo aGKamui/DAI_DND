044e11781dd627146cb1d47d4def770a4cae82b7
loadingpt <isaac-sousa@hotmail.com>
2023-05-28 13:32:21 +0000

    Add express session

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const authenticationRoutes = require("./route/authentication.route");
 const logger = require("./logger/api.logger");
@@ -12,13 +13,13 @@ app.use(bodyParser.json());
 
 
 // Configure session middleware
-// app.use(
-//   session({
-//     secret: 'qjF36qhSdXYYzfgnG9Z8sFSDaFYSPXym',     // Secret key used to sign the session ID cookie
-//     resave: false,                 // Whether to save the session to the store on every request
-//     saveUninitialized: false       // Whether to save uninitialized sessions to the store
-//   })
-// );
+app.use(
+  session({
+    secret: 'qjF36qhSdXYYzfgnG9Z8sFSDaFYSPXym',     // Secret key used to sign the session ID cookie
+    resave: false,                 // Whether to save the session to the store on every request
+    saveUninitialized: false       // Whether to save uninitialized sessions to the store
+  })
+);
 
 app.use("/api/authentication", authenticationRoutes);
 
