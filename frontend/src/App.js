e19a21d003cd9721ee89068139bab7e378c68cfc
jchry <jchrystellouk@gmail.com>
2023-05-31 15:39:21 +0000

    Routes + L + Ratio + Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate  } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -16,140 +16,17 @@ import MainLayout from './components/layout/MainLayout.tsx';
 import HomePage from './pages/home/HomePage.tsx';
 import { routes } from './routes/index.tsx';
 import $ from "jquery";
+import SignIn from './pages/login/LoginPage.js';
+import Register from './pages/login/RegisterPage.js';
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
-            {routes}
-        </Route>
+      <Routes>      
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<Register />}/>  
+        <Route path='*' element={<Navigate to="/login" replace={true} />} /> 
+         
       </Routes>
-    </BrowserRouter>
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
-export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-
-    $.ajax({
-      method: 'POST',
-      dataType: 'json',
-      url: 'http://localhost:8000/api/auth/login',
-      contentType: 'application/json',
-      headers: {
-        'Access-Control-Allow-Credentials' : true ,
-        'Access-Control-Allow-Origin' : '*' ,
-        'Access-Control-Allow-Methods' : 'GET,OPTIONS,PATCH,DELETE,POST,PUT' ,
-        'Access-Control-Allow-Headers' : 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization',
-    },
-      cache: false,
-      data: JSON.stringify({ username: data.get('email'), password: data.get('password') }),
-      success: function() {
-        <a
-          className="Home-link"
-          href="http://localhost:3000/dashboard"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-
-      },
-      error: function(xhr, status, error) { console.log(error); }
-    });
-
-  };
-
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Efetuar Login
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email de Utilizador"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Lembrar este computador"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
-}
-
-
-//function App() {
-//  return (
-//    <BrowserRouter>
-//      <div>
-//        <Routes>
-//          <Route path="/" element={<Home />} />
-//        </Routes>
-//      </div>
-//    </BrowserRouter>
-//  );
-//}
-
-// export default App;
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import SignIn from './pages/login/LoginPage.js';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals.js';
 import { CssBaseline } from '@mui/material';
 import { store } from './redux/store.ts';
-
+import App from './App';
+import { Route, Routes, BrowserRouter  } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
+  <BrowserRouter>
   <React.StrictMode>
     <Provider store={store}>
       <CssBaseline/>
       <App />
     </Provider>
   </React.StrictMode>
+  
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
