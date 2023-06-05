e4fa120d87005d6770cfdb9cabe6deb8259e4f2d
HenriqueMelo2002 <117111032+HenriqueMelo2002@users.noreply.github.com>
2023-06-05 16:58:59 +0000

    GridLoking louco

--- frontend/src/pages/dashboard/grid/GridPage.tsx
+++ frontend/src/pages/dashboard/grid/GridPage.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 import Draggable from "react-draggable";
+import $ from "jquery";
+
+import "./Grid.css"
+
+
+function createGrid(x) {
+  for (var rows = 0; rows < x; rows++) {
+      for (var columns = 0; columns < x; columns++) {
+          $("#container").append("<div class='grid'></div>");
+      };
+  };
+  $(".grid").width(960/x);
+  $(".grid").height(960/x);
+};
 
 export default function App() {
   const handleStart = (e, data) => {
@@ -11,27 +25,41 @@ export default function App() {
   const handleStop = (e, data) => {
     console.log(e, data);
   };
+
+  
+
+  
   return (
-    <Draggable
-      axis="both"
-      handle=".handle"
-      defaultPosition={{ x: 150, y: 0 }}
-      position={null}
-      grid={[150, 150]}
-      scale={1}
-      onStart={handleStart}
-      onDrag={handleDrag}
-      onStop={handleStop}
-    >
-      <div>
-        <div className="handle">Drag from here</div>
-        <div>Drag me.</div>
-      </div>
+
+    
+    <div className="grid-container">
+
+
+
+
+
+
+
+  <Draggable
+        axis="both"
+        handle=".handle"
+        defaultPosition={{ x: 0, y: 0 }}
+        position={null}
+        grid={[(window.innerWidth/30)-0.5, 51]}
+        scale={1}
+        onStart={handleStart}
+        onDrag={handleDrag}
+        onStop={handleStop}
+      >
+        <div>
+        <div className="handle"></div>  
+        </div>
       
 
-    </Draggable>
-    
+      </Draggable>
+    </div>
 
+    
     
   );
 }
