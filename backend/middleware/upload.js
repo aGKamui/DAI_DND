bcf026509bbbc2205d5b4bcaa5cea6f31bb02aaf
loadingpt <isaac-sousa@hotmail.com>
2023-06-09 11:52:02 +0000

    removed console.log test XD

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const multer = require('multer');
-console.log("test!")
 const storage = multer.diskStorage({
     destination: (req, file, cb) => { cb(null, "./public/images/") },
     filename: (req, file, cb) => { cb(null, Date.now() + path.extname(file.originalname)) }
